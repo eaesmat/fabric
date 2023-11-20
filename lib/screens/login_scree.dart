15e97a1dbca965993e57f37d0356ef83848ea91a
eaesmat <ea.ahmadzai2020@gmail.com>
2023-11-20 09:32:01 +0430

    Dry the code add widgets

--- lib/screens/login_scree.dart
+++ lib/screens/login_scree.dart
@@ -1,7 +1,6 @@
-import 'package:fabricproject/screens/app_languages_screen.dart';
 import 'package:fabricproject/theme/pallete.dart';
+import 'package:fabricproject/widgets/login_sreen_appbar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_locales/flutter_locales.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -10,31 +9,8 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Row(
-        children: [
-          ElevatedButton.icon(
-
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Pallete.whiteColor,
-              shadowColor: Colors.transparent,
-            ).copyWith(elevation:ButtonStyleButton.allOrNull(0.0)),
-            label: const LocaleText(
-              "language",
-              style: TextStyle(
-                  color: Pallete.blackColor, fontWeight: FontWeight.bold),
-            ),
-            icon: const Icon(
-              Icons.language_outlined,
-              color: Pallete.blackColor,
-            ),
-            onPressed: () => {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (_) => const AppLanguagesScreen()))
-            },
-          ),
-        ],
-      )),
-      body: const Center(child: Text("Home screen")),
+          title: const LoginScreenAppBar()),
+      body: const Center(child: Text("Home screen", style: TextStyle(color: Pallete.blueColor),)),
     );
   }
 }
